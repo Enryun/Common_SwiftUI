0b549dbddf845a6d2d2daeb360d1619d0ce4ee8c
JamesThang <jamesthang1996@gmail.com>
2024-08-10 17:57:39 +0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CommonSwiftUI",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CommonSwiftUI",
+            targets: ["CommonSwiftUI"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CommonSwiftUI",
+            path: "./Sources/CommonSwiftUI.xcframework")
+    ]
+)
---
+++ Sources/CommonSwiftUI.xcframework/ios-arm64_x86_64-simulator/CommonSwiftUI.framework/Headers/CommonSwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  CommonSwiftUI.h
+//  CommonSwiftUI
+//
+//  Created by James Thang on 02/06/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonSwiftUI.
+FOUNDATION_EXPORT double CommonSwiftUIVersionNumber;
+
+//! Project version string for CommonSwiftUI.
+FOUNDATION_EXPORT const unsigned char CommonSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonSwiftUI/PublicHeader.h>
+
+
